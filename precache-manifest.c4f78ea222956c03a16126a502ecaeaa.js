0556e379c96201b9c389db503440dcb94beafe1b
Miltosh <skalinovskiy94@gmail.com>
2020-10-03 01:24:36 +0300

    Updates

---
+++ precache-manifest.c4f78ea222956c03a16126a502ecaeaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a20d34bff3323207e360fcf71c074467",
+    "url": "/The-Game-Snake/index.html"
+  },
+  {
+    "revision": "e5ce9920549a0d59a6f4",
+    "url": "/The-Game-Snake/static/css/main.fcab5179.chunk.css"
+  },
+  {
+    "revision": "a929baf86d361d3a7deb",
+    "url": "/The-Game-Snake/static/js/2.1f96dfe0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/The-Game-Snake/static/js/2.1f96dfe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5ce9920549a0d59a6f4",
+    "url": "/The-Game-Snake/static/js/main.69c18812.chunk.js"
+  },
+  {
+    "revision": "88f6f4bc52dc096c4464",
+    "url": "/The-Game-Snake/static/js/runtime-main.1fd76f1c.js"
+  }
+]);
